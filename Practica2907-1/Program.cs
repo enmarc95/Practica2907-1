45ded09fcf67df4d512018658606290b6b1fbb04
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-07-29 09:24:36 -0600

    Primer programa

--- Practica2907-1/Program.cs
+++ Practica2907-1/Program.cs
@@ -6,7 +6,16 @@ namespace Practica2907_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaración de variables
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+
+            string name = "Enrique";
+            int edad = 27;
+            //Impresión de datos
+            Console.WriteLine("La suma es: " + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
