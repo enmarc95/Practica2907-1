c33adabc53f3c27bf86f6e5001ccd7976c59a590
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-07-29 09:50:48 -0600

    Segundo programa

---
+++ App2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables
+            string firstname = "Enrique";
+            String lastname = "Marenco";
+            int edad = 27;
+            //Concatenación de variables
+            var name = firstname + " " + lastname;
+            //Imprimir variables dentro del texto
+            Console.WriteLine("Su nombre es: {1} y su edad es: {0}", edad, name);
+        }
+    }
+}
