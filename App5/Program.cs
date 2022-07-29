891e0af7bf269984fb0b280fabb7776e671deb07
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-07-29 10:25:59 -0600

    Quinto programa

---
+++ App5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace App5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Introduzca un numero:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro numero:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los numeros es: " + (num1 + num2));
+            Console.WriteLine("La resta de los numeros es: " + (num1 - num2));
+        }
+    }
+}
