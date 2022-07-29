da01c48c745cfec6938df7be68f62a07be32f0aa
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-07-29 08:59:23 -0600

    Agregar archivos de proyecto.

---
+++ Practica2907-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica2907_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
