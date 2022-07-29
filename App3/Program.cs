018d7518bf9fe843fd8f31661a2740cc53254a26
Enrique Marenco <wilmer_marenco@hotmail.com>
2022-07-29 10:06:02 -0600

    Tercer programa

---
+++ App3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 52.24F;
+            double num2 = 52.24;
+            decimal num3 = 52.24M;
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+
+            /* Float --> 7 dígitos -- 32bits
+             * Double --> 15-16 dígitos -- 64bits
+             * Decimal --> 28-29 dígitos -- 128bits
+             */
+        }
+    }
+}
